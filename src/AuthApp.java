a6d9f112a3fe5bc9eb54a72661f51227fac77180
이재혁 <@DESKTOP-98664C2>
2020-08-26 19:51:49 +0900

    13열의 코드의 문제점이 뭘까?? 왜 equals 를 사용 해야하지?

--- src/AuthApp.java
+++ src/AuthApp.java
@@ -5,5 +5,17 @@ public class AuthApp {
 		
 		System.out.println(args[0]);
 		
+		String id = "egoing";
+		String inputId = args[0];
+		
+		System.out.println("Hi");
+		
+		//if(inputId==id) {
+		if(inputId.equals(id)) {
+			System.out.println("Master!");
+		}
+		else {
+			System.out.println("Who are you");
+		}
 	}
 }
